82c154791bf018ff32b469f79db4170877cedd5b
S1stem <islamovm988@gmail.com>
2024-03-19 22:42:29 +0300

    Update task_11.py

---
+++ task_11.py
@@ -0,0 +1,27 @@
+class Desert:
+    def __init__(self, name=None, calories=None):
+        if isinstance(name, str) and isinstance(calories, int):
+            self.name = name
+            self.calories = calories
+        else:
+            self.name = None
+            self.calories = None
+    @property
+    def name(self):
+        return self.__name
+    @name.setter
+    def name(self, value):
+        self.__name = value
+    @property
+    def calories(self):
+        return self.__calories
+    @calories.setter
+    def calories(self, value):
+        self.__calories = value
+    def ishealthy(self):
+        if self.calories <200:
+            return True
+        else:
+            return True
+    def isdelicious(self):      
+        return True
