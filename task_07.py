f385aaab8cf6553eaad035a7a02562040d2e893d
S1stem <islamovm988@gmail.com>
2024-03-19 22:42:22 +0300

    Update task_07.py

--- task_07.py
+++ task_07.py
@@ -1,8 +1,8 @@
 def combine_anagrams(words_array):
     anagram_dict = {}   
     for word in words_array:
-        sorted_word = sorted(word.lower())
-        if sorted_word in anagram_dict:
+        sorted_word = str(sorted(word.lower()))
+        if sorted_word in list(anagram_dict.keys()):
             anagram_dict[sorted_word].append(word)
         else:
             anagram_dict[sorted_word] = [word]
@@ -10,6 +10,6 @@ def combine_anagrams(words_array):
     return list(anagram_dict.values())
 
 # Пример использования
-words = ['listen', 'silent', 'enlist', 'hello', 'world']
-anagrams = combine_anagrams(words)
+anagrams = combine_anagrams(["cars", "for", "potatoes", "racs", "four", "scar",
+"creams", "scream"])
 print(anagrams)
