85cd42f65ca8ec6668a703f17095f11567f5c449
S1stem <islamovm988@gmail.com>
2024-03-19 22:42:13 +0300

    Update task_05.py

--- task_05.py
+++ task_05.py
@@ -2,7 +2,7 @@ import datetime
 def date_in_future(integer=None):
     if isinstance(integer, int):
         result= datetime.datetime.now() + datetime.timedelta(days=integer)
-        return result.strftime("%Y-%m-%d %H:%M:%S")        
+        return result.strftime("%d-%m-%Y %H:%M:%S")        
     else:
         return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-print(date_in_future(5))
+print(date_in_future([]))
