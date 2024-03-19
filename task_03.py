d9b1334acea591481b2857fe443fbb60852eea93
S1stem <islamovm988@gmail.com>
2024-03-19 22:42:05 +0300

    Update task_03.py

--- task_03.py
+++ task_03.py
@@ -2,10 +2,10 @@ import sys
 def max_odd(arr=[]):
     max=-sys.maxsize-1
     for element in arr:
-        if isinstance(element,(int,float)) and element>max:
+        if isinstance(element,(int,float)) and element>max and element%2==1:
             max=element
     if max==-sys.maxsize-1:
         return None
     else:
         return max
-print(max_odd(['ololo', 'fufufu']))
+print(max_odd([2, 2, 4]))
