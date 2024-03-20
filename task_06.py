bc52c05dd845f2e447708d9ceee3123a39941f4e
S1stem <islamovm988@gmail.com>
2024-03-20 14:51:02 +0300

    update task 6

--- task_06.py
+++ task_06.py
@@ -1,15 +1,26 @@
-
+class NoSuchStrategyError(Exception):
+    pass
+class WrongNumberOfPlayersError(Exception):
+    pass
 def rps_game_winner(arr):
-    elements="RPS"
-    if len(arr)!=2:
+    try:
+        elements="RPS"
+        if len(arr)!=2:
+            raise WrongNumberOfPlayersError("WrongNumberOfPlayersError")
+        for element in arr:
+            if element[1] not in elements:
+                raise NoSuchStrategyError("NoSuchStrategyError")
+        if arr[0][1]==arr[1][1]:
+            return "Player1"+" "+arr[0][1]
+        if (arr[0][1]=="R" and arr[1][1]=="S") or (arr[0][1]=="S" and arr[1][1]=="P") or (arr[0][1]=="P" and arr[1][1]=="R"):
+            return "Player1"+" "+arr[0][1]
+        else:
+            return "Player2"+" "+arr[1][1]
+    except NoSuchStrategyError:
+        return "WrongStrategyError"
+    except WrongNumberOfPlayersError:
         return "WrongNumberOfPlayersError"
-    for element in arr:
-        if element[1] not in elements:
-            return "NoSuchStrategyError"
-    if arr[0][1]==arr[1][1]:
-        return "Player1"+" "+arr[0][1]
-    if (arr[0][1]=="R" and arr[1][1]=="S") or (arr[0][1]=="S" and arr[1][1]=="P") or (arr[0][1]=="P" and arr[1][1]=="R"):
-        return "Player1"+" "+arr[0][1]
-    else:
-        return "Player2"+" "+arr[1][1]
-print(rps_game_winner([['player1', 'P'], ['player2', 'A'],[]]))
+print(rps_game_winner([['player1', 'P'], ['player2', 'A'],]))
+print(rps_game_winner([['player1', 'R'], ['player2', 'S'],]))
+print(rps_game_winner([['player1', 'R'], ['player2', 'R'],]))
+print(rps_game_winner([['player1', 'R'], ['player2', 'P'],[]]))
