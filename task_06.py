cc16896c2a2852df6e10e3fdd8b073a61b72b060
S1stem <islamovm988@gmail.com>
2024-03-20 14:54:26 +0300

    Update task_06.py

--- task_06.py
+++ task_06.py
@@ -6,10 +6,10 @@ def rps_game_winner(arr):
     try:
         elements="RPS"
         if len(arr)!=2:
-            raise WrongNumberOfPlayersError("WrongNumberOfPlayersError")
+            raise WrongNumberOfPlayersError()
         for element in arr:
             if element[1] not in elements:
-                raise NoSuchStrategyError("NoSuchStrategyError")
+                raise NoSuchStrategyError()
         if arr[0][1]==arr[1][1]:
             return "Player1"+" "+arr[0][1]
         if (arr[0][1]=="R" and arr[1][1]=="S") or (arr[0][1]=="S" and arr[1][1]=="P") or (arr[0][1]=="P" and arr[1][1]=="R"):
